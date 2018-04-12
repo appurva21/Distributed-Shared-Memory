84d560559be48ad1268e71acb39e9ecc9d2b4dbb
Appurva Murawat <appurva21@gmail.com>
2018-04-12 09:07:46 +0530

    Project Completed

--- src/MessageWindow.java
+++ src/MessageWindow.java
@@ -2,16 +2,6 @@
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author himanshu2936
- */
 public class MessageWindow extends JFrame{
     
     
